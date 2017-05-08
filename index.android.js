1aac3282f62e5798f9fe4d153c4e2367f9d8bddb
Aria Moraine <ariamoraine@gmail.com>
2017-05-08 11:42:31 -0400

    working on rendering more components

--- index.android.js
+++ index.android.js
@@ -12,46 +12,10 @@ import {
   View,
   TouchableHighlight,
   Alert,
-  Button
+  Button,
+  Image,
+  Dimensions
 } from 'react-native';
-
-export default class littleChef extends Component {
-
-  _onPressButton () {
-    Alert.alert("You clicked it ");
-  }
-
-  testFunctionHere () {
-    return (
-      <Text>Somthing here</Text>
-    )
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Little Chef!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-        <TouchableHighlight onPress={this._onPressButton}>
-          <Text>Button</Text>
-        </TouchableHighlight>
-        <Button
-          onPress={this.testFunctionHere}
-          title="Test Button here"
-          color="#841584"
-        />
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -70,6 +34,57 @@ const styles = StyleSheet.create({
     color: '#FF8E30',
     marginBottom: 5,
   },
+  // picture: {
+  //   width: '50vh',
+  //   height: '50vw',
+  // }
 });
 
+class TestingCall extends Component {
+  render(){
+    return (
+      <Text>SOME TEXT HERE</Text>
+    )
+  }
+}
+
+export default class littleChef extends Component {
+
+  _onPressButton () {
+    Alert.alert("You clicked it ");
+      <TestingCall />
+  }
+
+  render() {
+
+    // let testPic = {uri: "http://images4.fanpop.com/image/photos/22400000/Cute-Kitten-kittens-22438020-480-360.jpg"};
+    // let {height, width} = Dimensions.get('window');
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Little Chef!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+        <TouchableHighlight onPress={this.onPress}>
+          <Text>Button</Text>
+        </TouchableHighlight>
+        <Button
+          onPress={this._onPressButton}
+          title="Test Button here"
+          color="#841584"
+        />
+        <TestingCall />
+      </View>
+    );
+  }
+}
+
+//<Image source={testPic} style={{height, width}} />
 AppRegistry.registerComponent('littleChef', () => littleChef);
