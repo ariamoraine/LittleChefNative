f3e8781a9dc780cc9502b5f863a5c0a420f839bf
Aria Moraine <ariamoraine@gmail.com>
2017-05-12 14:42:16 -0400

    adding in text field that saves to the phone

--- App.js
+++ App.js
@@ -38,14 +38,18 @@ class AllRecipes extends React.Component {
     constructor (props) {
       super(props);
       this.state = {
-        recipes: ""
+        recipes: "",
+        textToUpdate: '',
       }
     }
 
     componentDidMount () {
       AsyncStorage.getItem('recipes')
       .then(returnedRecipes => {
-        this.setState({'recipes': returnedRecipes})
+        console.log("Just got the info from the DB and it is", returnedRecipes)
+        this.setState({
+          recipes: returnedRecipes
+        })
       })
       .done();
     }
@@ -54,32 +58,58 @@ class AllRecipes extends React.Component {
     //   return {'recipes': " "};
     // }
 
-    saveData (value) {
-      AsyncStorage.setItem('recipes', value);
-      this.setState({"recipes": this.state.recipes + value});
+    saveData (text) {
+      console.log("Inside data", text)
+      AsyncStorage.setItem('recipes', this.state.textToUpdate)
+      .then((someStuff) => {
+        console.log('Inside save data', someStuff);
+      })
+      .done()
+      // this.setState({"recipes": this.state.textToUpdate + value});
     }
 
   render() {
     console.log(this.state.recipes)
     let recipes = this.state.recipes || " ";
     const { navigate } = this.props.navigation;
+
     return (
       <View>
         <Text>All your recipes</Text>
-        <Text>{this.state.recipes}</Text>
+        <Text>{`Recipes: ${this.state.recipes}`}</Text>
+        <Text>{`TextToUpdate: ${this.state.TextToUpdate}`}</Text>
         <View>
           <TextInput
-            onChangeText={text => this.saveData(text)}
-            value=""
+            onChangeText={text => {
+              console.log("RECIPES", this.state.recipes);
+              console.log("TextToUpdate", this.state.textToUpdate);
+              console.log("In onChange text is ->", text)
+              if (this.state.textToUpdate === null) {
+                console.log('Inside if in onChange')
+                this.setState({
+                  textToUpdate: text
+                })
+              } else {
+                console.log('Inside else in onChange')
+                this.setState({
+                  textToUpdate: this.state.textToUpdate += text
+                })
+              }
+            }}
+            value={this.state.textToUpdate}
           />
         </View>
         <Text>
           Next time we open it will load the saved data
         </Text>
         <Button
-          // onPress=saveData()
-          onPress={() => navigate('AddRecipe')}
-          title="Add a new recipe!"
+          onPress={() => {
+            console.log("In onpress", this.state.textToUpdate)
+            this.saveData(this.state.textToUpdate)
+          }}
+          title="Save"
+          // onPress={() => navigate('AddRecipe')}
+          // title="Add a new recipe!"
         />
       </View>
     );
