635992855b047bfbbe5eebcc0907f2ca36c7cef4
Aria Moraine <ariamoraine@gmail.com>
2017-05-19 17:19:02 -0400

    title, directions, and ingredient inputs working

--- Components/AddRecipe.js
+++ Components/AddRecipe.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { View, Button, Text, TextInput, StyleSheet} from 'react-native';
 
-  const styles = StyleSheet.create({
-    lines: {
-      borderTopWidth: 1,
-      borderBottomWidth: 1,
-      borderColor: 'black',
-      width: 400,
-    }
-  })
+const styles = StyleSheet.create({
+  lines: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    width: 400,
+  }
+});
 
 export default class AddRecipe extends Component {
 
@@ -22,14 +22,14 @@ export default class AddRecipe extends Component {
       inputText: ''
     };
     this.handleInput = this.handleInput.bind(this);
+    this.handleIngInput = this.handleIngInput.bind(this);
   }
 
-  addIngredientButton (ingredient) {
-    //this button is called when user wants to add another ingredient to
-    //their recipe
-    //it takes the current ingredient,
-    //pushes that into the allIngredient array
-    //clears the current ingredient so they can add more if wanted.
+  handleIngInput () {
+    this.setState({
+      allIngredients: [...this.state.allIngredients, this.state.currentIngredient],
+      currentIngredient: ''
+    });
   }
 
   handleInput (text, type) {
@@ -40,8 +40,7 @@ export default class AddRecipe extends Component {
   }
 
   render () {
-    console.log('state text', this.state);
-    console.log('state. text to update', this.state.textToUpdate)
+
     return (
       <View>
         <Text>Add a recipe here!</Text>
@@ -58,26 +57,35 @@ export default class AddRecipe extends Component {
         {/* Ingredient input area */}
         <View
           style={styles.lines}>
-          <Text>Ingredients :</Text>
+          <Text>Ingredients List :</Text>
+          {this.state.allIngredients.map((ing, index) => <Text key={index}>{` -> ${ing}`}</Text>)}
           <TextInput
-            onChangeText={(ingredient) => this.handleInput(ingredient, "ingredients")}
-            value={this.state.ingredients}
+            onChangeText={(ingredient) => this.handleInput(ingredient, "currentIngredient")}
+            value={this.state.currentIngredient}
+          />
+          <Button
+            title="ADD"
+            onPress={this.handleIngInput}
           />
         </View>
 
         {/* Directions input area */}
         <View
           style={styles.lines}>
-          <Text>Directions :</Text>
           <TextInput
+            placeholder="Directions"
             onChangeText={(text) => this.handleInput(text, "directions")}
             value={this.state.directions}
             multiline={true}
           />
         </View>
+      {
+        /* Save recipe button //should build the recipe object and save it to the db
+        and route us back to the main recipe page where the new recipe will display */
+      }
         <Button
           onPress={() => this.props.navigation.navigate('AllRecipes')}
-          title="Save and add"
+          title="SAVE AND ADD"
         />
       </View>
     );
